f319cfb7e45affd180673d9c5ca05e2c2dbf09c3
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-11-20 17:17:08 +0300

    group-multilevel js and css

--- sugar_office_worker/ilknur/App.js
+++ sugar_office_worker/ilknur/App.js
@@ -29,61 +29,20 @@
     });
 });
 
-// Yeni eklenen butonlar için kodlar
-const multilevelButton = document.querySelector('.group-multilevel button');
-const multilevelMenu = document.querySelector('.group-multilevel .absolute');
-
-multilevelButton.addEventListener('click', function () {
-    multilevelMenu.classList.toggle('hidden');
-});
-
-document.addEventListener('click', function (event) {
-    if (!multilevelButton.contains(event.target)) {
-        multilevelMenu.classList.add('hidden');
-    }
-});
-
-const brandsButton = document.querySelector('.group-brands button');
-const brandsMenu = document.querySelector('.group-brands .absolute');
-
-brandsButton.addEventListener('click', function () {
-    brandsMenu.classList.toggle('hidden');
-});
-
-document.addEventListener('click', function (event) {
-    if (!brandsButton.contains(event.target)) {
-        brandsMenu.classList.add('hidden');
-    }
-});
-
-const specialsButton = document.querySelector('.group-specials button');
-const specialsMenu = document.querySelector('.group-specials .absolute');
-
-specialsButton.addEventListener('click', function () {
-    specialsMenu.classList.toggle('hidden');
-});
-
-document.addEventListener('click', function (event) {
-    if (!specialsButton.contains(event.target)) {
-        specialsMenu.classList.add('hidden');
-    }
-});
-
-
 document.addEventListener('DOMContentLoaded', function () {
-    const categoriesButton = document.querySelector('.group-multilevel .group:nth-child(1) a');
+    const categoriesButton = document.querySelector('.group-multilevel .group:nth-child(1) button');
     const categoriesMenu = document.querySelector('.group-multilevel .group:nth-child(1) .absolute');
 
-    const megaMenuButton = document.querySelector('.group-multilevel .group:nth-child(2) a');
+    const megaMenuButton = document.querySelector('.group-multilevel .group:nth-child(2) button');
     const megaMenu = document.querySelector('.group-multilevel .group:nth-child(2) .absolute');
 
-    const multiLevelButton = document.querySelector('.group-multilevel .group:nth-child(3) a');
+    const multiLevelButton = document.querySelector('.group-multilevel .group:nth-child(3) button');
     const multiLevelMenu = document.querySelector('.group-multilevel .group:nth-child(3) .absolute');
 
-    const brandsButton = document.querySelector('.group-multilevel .group:nth-child(4) a');
+    const brandsButton = document.querySelector('.group-multilevel .group:nth-child(4) button');
     const brandsMenu = document.querySelector('.group-multilevel .group:nth-child(4) .absolute');
 
-    const specialsButton = document.querySelector('.group-multilevel .group:nth-child(5) a');
+    const specialsButton = document.querySelector('.group-multilevel .group:nth-child(5) button');
     const specialsMenu = document.querySelector('.group-multilevel .group:nth-child(5) .absolute');
 
     function hideAllMenus() {
@@ -94,32 +53,27 @@ document.addEventListener('DOMContentLoaded', function () {
         specialsMenu.classList.add('hidden');
     }
 
-    categoriesButton.addEventListener('click', function (event) {
-        event.preventDefault();
+    categoriesButton.addEventListener('click', function () {
         hideAllMenus();
         categoriesMenu.classList.toggle('hidden');
     });
 
-    megaMenuButton.addEventListener('click', function (event) {
-        event.preventDefault();
+    megaMenuButton.addEventListener('click', function () {
         hideAllMenus();
         megaMenu.classList.toggle('hidden');
     });
 
-    multiLevelButton.addEventListener('click', function (event) {
-        event.preventDefault();
+    multiLevelButton.addEventListener('click', function () {
         hideAllMenus();
         multiLevelMenu.classList.toggle('hidden');
     });
 
-    brandsButton.addEventListener('click', function (event) {
-        event.preventDefault();
+    brandsButton.addEventListener('click', function () {
         hideAllMenus();
         brandsMenu.classList.toggle('hidden');
     });
 
-    specialsButton.addEventListener('click', function (event) {
-        event.preventDefault();
+    specialsButton.addEventListener('click', function () {
         hideAllMenus();
         specialsMenu.classList.toggle('hidden');
     });
@@ -139,27 +93,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-// // JavaScript kullanarak accordion özelliği ekleme
-// document.addEventListener('DOMContentLoaded', function () {
-// var buttons = document.querySelectorAll('button[data-target]');
 
-// buttons.forEach(function (button) {
-//     button.addEventListener('click', function () {
-//         var targetId = this.getAttribute('data-target');
-//         var targetElement = document.getElementById(targetId);
 
-//         // Diğer tüm menüleri gizle
-//         buttons.forEach(function (btn) {
-//             var otherTargetId = btn.getAttribute('data-target');
-//             if (otherTargetId !== targetId) {
-//                 document.getElementById(otherTargetId).classList.add('hidden');
-//             }
-//         });
-
-//         // Tıklanan menüyü göster veya gizle
-//         targetElement.classList.toggle('hidden');
-//     });
-// });
-// });
 
 

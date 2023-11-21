405e1e2d34dc066ead46fe64fba09c17818e2119
ykcsoftwarer <yusufkucukcopur@gmail.com>
2023-11-22 01:05:36 +0300

    slider finished

--- sugar_office_worker/Joseph/assets/js/sugar.js
+++ sugar_office_worker/Joseph/assets/js/sugar.js
@@ -1,37 +1,37 @@
 //! ------------top menu
-document.addEventListener("DOMContentLoaded", function () {
-  // Function to toggle the visibility of the dropdown
-  function toggleDropdown(dropdown, isOpen) {
-    dropdown.style.display = isOpen ? "block" : "none";
-  }
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Function to toggle the visibility of the dropdown
+//   function toggleDropdown(dropdown, isOpen) {
+//     dropdown.style.display = isOpen ? "block" : "none";
+//   }
 
-  // Function to handle mouseenter and mouseleave events
-  function handleMouseEvents(trigger, dropdown) {
-    trigger.addEventListener("mouseenter", function () {
-      toggleDropdown(dropdown, true);
-    });
+//   // Function to handle mouseenter and mouseleave events
+//   function handleMouseEvents(trigger, dropdown) {
+//     trigger.addEventListener("mouseenter", function () {
+//       toggleDropdown(dropdown, true);
+//     });
 
-    trigger.addEventListener("mouseleave", function () {
-      toggleDropdown(dropdown, false);
-    });
-  }
+//     trigger.addEventListener("mouseleave", function () {
+//       toggleDropdown(dropdown, false);
+//     });
+//   }
 
-  // Buy JOURNAL dropdown
-  const buyJournalTrigger = document.querySelector(".all-header .language");
-  const buyJournalDropdown = document.querySelector(".all-header .language .absolute");
+//   // Buy JOURNAL dropdown
+//   const buyJournalTrigger = document.querySelector(".all-header .language");
+//   const buyJournalDropdown = document.querySelector(".all-header .language .absolute");
 
-  if (buyJournalTrigger && buyJournalDropdown) {
-    handleMouseEvents(buyJournalTrigger, buyJournalDropdown);
-  }
+//   if (buyJournalTrigger && buyJournalDropdown) {
+//     handleMouseEvents(buyJournalTrigger, buyJournalDropdown);
+//   }
 
-  // Currency dropdown
-  const currencyTrigger = document.querySelector(".all-header .currency");
-  const currencyDropdown = document.querySelector(".all-header .currency .absolute");
+//   // Currency dropdown
+//   const currencyTrigger = document.querySelector(".all-header .currency");
+//   const currencyDropdown = document.querySelector(".all-header .currency .absolute");
 
-  if (currencyTrigger && currencyDropdown) {
-    handleMouseEvents(currencyTrigger, currencyDropdown);
-  }
-});
+//   if (currencyTrigger && currencyDropdown) {
+//     handleMouseEvents(currencyTrigger, currencyDropdown);
+//   }
+// });
 
 // !---------------------------
 

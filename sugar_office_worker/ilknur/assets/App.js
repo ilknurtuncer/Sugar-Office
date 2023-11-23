9e6a219a4cc33b18eb300f9d630a1ae7aac29771
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-11-23 18:08:30 +0300

    links hover and Multilevel button mouseover-click

--- sugar_office_worker/ilknur/assets/App.js
+++ sugar_office_worker/ilknur/assets/App.js
@@ -29,89 +29,106 @@
     });
 });
 
-//-Group Multilevel button -----
+//-Group Multilevel button 
+
+
 document.addEventListener('DOMContentLoaded', function () {
-    const categoriesButton = document.querySelector('.group-multilevel .group:nth-child(1) button');
-    const categoriesMenu = document.querySelector('.group-multilevel .group:nth-child(1) .absolute');
+    // Menü öğelerini seç
+    const menuItems = document.querySelectorAll('.group-multilevel .group');
 
-    const megaMenuButton = document.querySelector('.group-multilevel .group:nth-child(2) button');
-    const megaMenu = document.querySelector('.group-multilevel .group:nth-child(2) .absolute');
-
-    const multiLevelButton = document.querySelector('.group-multilevel .group:nth-child(3) button');
-    const multiLevelMenu = document.querySelector('.group-multilevel .group:nth-child(3) .absolute');
-
-    const brandsButton = document.querySelector('.group-multilevel .group:nth-child(4) button');
-    const brandsMenu = document.querySelector('.group-multilevel .group:nth-child(4) .absolute');
-
-    const specialsButton = document.querySelector('.group-multilevel .group:nth-child(5) button');
-    const specialsMenu = document.querySelector('.group-multilevel .group:nth-child(5) .absolute');
-
+    // Tüm menüleri gizleme fonksiyonu
     function hideAllMenus() {
-        categoriesMenu.classList.add('hidden');
-        megaMenu.classList.add('hidden');
-        multiLevelMenu.classList.add('hidden');
-        brandsMenu.classList.add('hidden');
-        specialsMenu.classList.add('hidden');
+        // Her bir menü öğesini döngüye al
+        menuItems.forEach(item => {
+            // Menüyü seç
+            const menu = item.querySelector('.absolute');
+            // Menüyü gizle
+            menu.classList.add('hidden');
+        });
     }
 
-    categoriesButton.addEventListener('mouseover', function () {
-        hideAllMenus();
-        categoriesMenu.classList.toggle('hidden');
+    // Her bir menü öğesi için olay dinleyicileri ekle
+    menuItems.forEach(item => {
+        // Menü düğmesini seç
+        const button = item.querySelector('button');
+        // Menüyü seç
+        const menu = item.querySelector('.absolute');
+
+        // Düğmeye gelindiğinde
+        button.addEventListener('mouseover', function () {
+            // Diğer menüleri gizle
+            hideAllMenus();
+            // Bu menüyü göster
+            menu.classList.remove('hidden');
+        });
+
+        // Menüden çıkıldığında
+        menu.addEventListener('mouseleave', function () {
+            // Menüyü gizle
+            menu.classList.add('hidden');
+        });
     });
 
-    megaMenuButton.addEventListener('mouseover', function () {
-        hideAllMenus();
-        megaMenu.classList.toggle('hidden');
-    });
-
-    multiLevelButton.addEventListener('mouseover', function () {
-        hideAllMenus();
-        multiLevelMenu.classList.toggle('hidden');
-    });
-
-    brandsButton.addEventListener('mouseover', function () {
-        hideAllMenus();
-        brandsMenu.classList.toggle('hidden');
-    });
-
-    specialsButton.addEventListener('mouseover', function () {
-        hideAllMenus();
-        specialsMenu.classList.toggle('hidden');
-    });
-
-    document.addEventListener('mouseover', function (event) {
-        if (
-            !categoriesButton.contains(event.target) &&
-            !megaMenuButton.contains(event.target) &&
-            !multiLevelButton.contains(event.target) &&
-            !brandsButton.contains(event.target) &&
-            !specialsButton.contains(event.target)
-        ) {
+    // Doküman seviyesinde bir tıklama olayı ekle
+    document.addEventListener('click', function (event) {
+        // Tıklanan öğenin bir düğme olup olmadığını kontrol et
+        const isButtonClick = event.target.tagName === 'BUTTON';
+        // Tıklanan öğenin bir bağlantı olup olmadığını kontrol et
+        const isLinkClick = event.target.tagName === 'A';
+        
+        // Eğer tıklanan öğe hem düğme hem de bağlantı değilse
+        if (!isButtonClick && !isLinkClick) {
+            // Tüm menüleri gizle
             hideAllMenus();
         }
     });
 });
-// sepet------
-document.addEventListener("DOMContentLoaded", function () {
-    var cartButton = document.getElementById("cart-button");
-    var cartDropdown = document.getElementById("cart-dropdown");
 
-    // Sepet butonuna tıklandığında
-    cartButton.addEventListener("mouseover", function () {
-        // Sepet dropdown'un görünürlüğünü toggle et
-        cartDropdown.classList.toggle("hidden");
+
+
+
+ // Sepet -------
+
+ document.addEventListener("DOMContentLoaded", function () {
+    // Genel değişkenler
+    const dropdownButtons = document.querySelectorAll('.dropdown-button');
+    const dropdowns = document.querySelectorAll('.dropdown');
+
+    // Dropdown'ları gizleme fonksiyonu
+    function hideAllDropdowns() {
+        dropdowns.forEach(dropdown => {
+            dropdown.classList.add("hidden");
+        });
+    }
+
+    // Her dropdown butonu için olay dinleyicisi ekleme
+    dropdownButtons.forEach(button => {
+        button.addEventListener("mouseover", function () {
+            hideAllDropdowns();
+            const dropdown = button.nextElementSibling;
+            dropdown.classList.remove("hidden");
+        });
     });
 
-    // Örnek: Ürün eklemek için bir bağlantıya tıklandığında
-    var addToCartLinks = document.querySelectorAll("#cart-dropdown a");
-    addToCartLinks.forEach(function (link) {
-        link.addEventListener("mouseover", function (event) {
+    // Document seviyesinde bir tıklama olayı ekleyerek dropdownları kapatma
+    document.addEventListener("click", function (event) {
+        if (!event.target.classList.contains('dropdown-button') && !event.target.classList.contains('dropdown')) {
+            hideAllDropdowns();
+        }
+    });
+
+    // Ürün eklemek için bağlantılara tıklama olayı ekleme
+    const addToCartLinks = document.querySelectorAll('.dropdown a');
+    addToCartLinks.forEach(link => {
+        link.addEventListener("click", function (event) {
             event.preventDefault();
             // Burada ürün eklemek için gerekli işlemleri yapabilirsiniz.
             console.log("Ürün eklendi: " + link.textContent);
         });
     });
 });
+
+
 
 
 

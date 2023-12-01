c564b08452d9f66719c06806d749d9cd33aaa1b0
Talha Seven <131260032+TalhaSeven@users.noreply.github.com>
2023-12-01 23:12:15 +0300

    Remove commented out code and optimize image slider

--- sugar_office_worker/Talha/assets/js/main.js
+++ sugar_office_worker/Talha/assets/js/main.js
@@ -1,33 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const slides = document.querySelectorAll("#slider .slide");
-//   let currentSlide = 1;
-//   function fadeToSlide(slideIndex) {
-//     slides[currentSlide].classList.add("opacity-0");
-//     slides[currentSlide].classList.remove("opacity-100");
-//     currentSlide = (slideIndex + slides.length) % slides.length;
-//     slides[currentSlide].classList.add("opacity-100");
-//     slides[currentSlide].classList.remove("opacity-0");
-//   }
-
-//   document.getElementById("prev").addEventListener("click", () => {
-//     fadeToSlide(currentSlide - 1);
-//   });
-
-//   document.getElementById("next").addEventListener("click", () => {
-//     fadeToSlide(currentSlide + 1);
-//   });
-
-//   // setInterval(() => {
-//   //   fadeToSlide(currentSlide + 1);
-//   // }, 2000);
-// });
-
-// document.getElementById('menu-btn').addEventListener('click', function() {
-//   var menu = document.getElementById('menu');
-//   menu.classList.toggle('hidden');
-// });
-
-
 let defaultTransform = 0;
 function goNext() {
     defaultTransform = defaultTransform - 398;

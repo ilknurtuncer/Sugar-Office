818d2d9ee0e1b02aafc0d614ddd4f7ddd3a4c4b9
Ahme Koçoğlu <akocoglu@gmail.com>
2023-12-07 15:36:55 +0300

    Delete sugar_office_worker directory

--- sugar_office_worker/Salih/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./*.{html,js}",  "./node_modules/flowbite/**/*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [ 'flowbite/plugin'],
-}
-
